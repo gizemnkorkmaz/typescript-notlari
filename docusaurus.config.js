f5b4501c54bdad1cb6cccc38f8e8639dc01957bc
gizemnkorkmaz <gizemkorkmaz@teknasyon.com>
2023-03-01 12:06:27 +0300

    chore: clean up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,13 +44,6 @@ const config = {
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-        },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
@@ -64,7 +57,6 @@ const config = {
       // Replace with your project's social card
       image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: "My Site",
         logo: {
           alt: "My Site Logo",
           src: "img/logo.svg",
@@ -74,19 +66,23 @@ const config = {
             type: "doc",
             docId: "intro",
             position: "left",
-            label: "Tutorial",
+            label: "Notes",
           },
-          { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/gizemnkorkmaz/ts-notes",
             label: "GitHub",
+            position: "right",
+          },
+          {
+            href: "https://twitter.com/gizemnkorkmaz",
+            label: "Twitter",
             position: "right",
           },
         ],
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: darkCodeTheme,
+        darkTheme: lightCodeTheme,
       },
     }),
 };
